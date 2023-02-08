8567d6bb11354a056989c716b026aab01813e22d
Witold Hungendorfer <witekh@me.com>
2023-02-08 12:56:16 +0100

    Add value check

--- calculate.py
+++ calculate.py
@@ -1,17 +1,26 @@
 import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
-x = float(input("Podaj składnik 1. "))
-y = float(input("Podaj składnik 2. "))
-act = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
-if act == '1':
+while True:
+  try:
+    x = float(input("Podaj składnik 1. "))
+    y = float(input("Podaj składnik 2. "))
+  except ValueError:
+    print("Błąd nieprawidłowy składnik. ")
+    continue
+  act = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+  if act == '1':
     logging.info("Dodaję %s i %s" %(x,y))
     print(x+y)
-elif act == '2':
-    logging.info(" Odejmuję od %s  %s" %(x,y))
+    break
+  elif act == '2':
+    logging.info("Odejmuję od %s  %s" %(x,y))
     print(x-y)
-elif act == '3':
-    logging.info(" Mnożę %s i %s" %(x,y))
+    break
+  elif act == '3':
+    logging.info("Mnożę %s i %s" %(x,y))
     print(x*y)
-elif act == '4':
-    logging.info(" Dzielę %s przez %s" %(x,y))
+    break
+  elif act == '4':
+    logging.info("Dzielę %s przez %s" %(x,y))
     print(x/y)
+    break
