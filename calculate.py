f69da512be26e810ed867e3ace335f8fa6b3dbb3
Witold Hungendorfer <witekh@me.com>
2023-02-09 00:13:10 +0100

    Add function

--- calculate.py
+++ calculate.py
@@ -1,4 +1,20 @@
 import logging
+  
+def calculate(act, num_1, num_2):
+  if act == '1':
+    logging.info("Dodaję %s i %s" %(x,y))
+    print(x+y)
+  elif act == '2':
+    logging.info("Odejmuję od %s  %s" %(x,y))
+    print(x-y)
+  elif act == '3':
+    logging.info("Mnożę %s i %s" %(x,y))
+    print(x*y)
+  elif act == '4':
+    logging.info("Dzielę %s przez %s" %(x,y))
+    print(x/y)
+    
+
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 act = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
 while True:
@@ -8,20 +24,5 @@ while True:
   except ValueError:
     print("Błąd nieprawidłowy składnik. ")
     continue
-  
-  if act == '1':
-    logging.info("Dodaję %s i %s" %(x,y))
-    print(x+y)
-    break
-  elif act == '2':
-    logging.info("Odejmuję od %s  %s" %(x,y))
-    print(x-y)
-    break
-  elif act == '3':
-    logging.info("Mnożę %s i %s" %(x,y))
-    print(x*y)
-    break
-  elif act == '4':
-    logging.info("Dzielę %s przez %s" %(x,y))
-    print(x/y)
-    break
+  break
+calculate(act, x, y)
