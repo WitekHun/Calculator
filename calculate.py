abf29d7c61c0c089548d38951e3ae27cae9acf36
Witold Hungendorfer <witekh@me.com>
2023-02-08 16:22:19 +0100

    Change order of user input

--- calculate.py
+++ calculate.py
@@ -1,5 +1,6 @@
 import logging
 logging.basicConfig(level=logging.INFO, format='%(message)s')
+act = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
 while True:
   try:
     x = float(input("Podaj składnik 1. "))
@@ -7,7 +8,7 @@ while True:
   except ValueError:
     print("Błąd nieprawidłowy składnik. ")
     continue
-  act = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+  
   if act == '1':
     logging.info("Dodaję %s i %s" %(x,y))
     print(x+y)
