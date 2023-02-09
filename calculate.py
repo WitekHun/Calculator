9a3ceef3764619084de61956bc9677c871d6e94c
Witold Hungendorfer <witekh@me.com>
2023-02-09 12:26:04 +0100

    Use black to format

--- calculate.py
+++ calculate.py
@@ -1,23 +1,26 @@
 import logging
-  
-def calculate(act, x, y):
-    if act == '1':
-        logging.info("Dodaję %s i %s" %(x,y))
-        print("Wynik to: ", x+y)
-    elif act == '2':
-        logging.info("Odejmuję od %s  %s" %(x,y))
-        print("Wynik to: ", x-y)
-    elif act == '3':
-        logging.info("Mnożę %s i %s" %(x,y))
-        print("Wynik to: ", x*y)
-    elif act == '4':
-        logging.info("Dzielę %s przez %s" %(x,y))
-        print("Wynik to: ", x/y)
 
-    
-if __name__=="__main__":
-    logging.basicConfig(level=logging.INFO, format='%(message)s')
-    act = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
+
+def calculate(act, x, y):
+    if act == "1":
+        logging.info("Dodaję %s i %s" % (x, y))
+        print("Wynik to: ", x + y)
+    elif act == "2":
+        logging.info("Odejmuję od %s  %s" % (x, y))
+        print("Wynik to: ", x - y)
+    elif act == "3":
+        logging.info("Mnożę %s i %s" % (x, y))
+        print("Wynik to: ", x * y)
+    elif act == "4":
+        logging.info("Dzielę %s przez %s" % (x, y))
+        print("Wynik to: ", x / y)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
+    act = input(
+        "Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: "
+    )
     while True:
         try:
             num_1 = float(input("Podaj składnik 1. "))
@@ -26,4 +29,4 @@ if __name__=="__main__":
             print("Błąd nieprawidłowy składnik. ")
             continue
         break
-    calculate(act, num_1,num_2)
+    calculate(act, num_1, num_2)
