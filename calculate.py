e6c3639eecf69981e98cb2e91e8d4821d2b6fe0e
Witold Hungendorfer <witekh@me.com>
2023-02-09 12:10:16 +0100

    Fix veriables

--- calculate.py
+++ calculate.py
@@ -1,6 +1,6 @@
 import logging
   
-def calculate(act, num_1, num_2):
+def calculate(act, x, y):
   if act == '1':
     logging.info("Dodaję %s i %s" %(x,y))
     print("Wynik to: ", x+y)
@@ -19,10 +19,10 @@ if __name__=="__main__":
   act = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: ")
   while True:
     try:
-      x = float(input("Podaj składnik 1. "))
-      y = float(input("Podaj składnik 2. "))
+      num_1 = float(input("Podaj składnik 1. "))
+      num_2 = float(input("Podaj składnik 2. "))
     except ValueError:
       print("Błąd nieprawidłowy składnik. ")
       continue
     break
-  calculate(act, x, y)
+  calculate(act, num_1,num_2)
